c9c7e2a6a658183539763bbcc75291286d2d230c
Amman Abua <ammanabua@gmail.com>
2020-11-11 10:03:07 +0100

    break app into modules and require them

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const User = require("./models/User");
 
 const { MONGODB } = require("./config.js");
 
-const typeDefs = gql`
-    type Query{
-        sayHi: String!
-    }
-`;
 
-const resolvers = {
-    Query: {
-        sayHi: () => "Hello World!"
-    }
-};
 
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
+ 
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology:true })
 .then(() => {
