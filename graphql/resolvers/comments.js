c2724e277bf5327467613c1156469a365a2548f9
Amman Abua <ammanabua@gmail.com>
2020-11-20 14:36:40 +0100

    add likes as a toggle functionality

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -47,6 +47,28 @@ module.exports = {
             } else {
                 throw new UserInputError("Post not found");
             }
+        },
+        async likePost(_, { postId }, context){
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    //Post already like so we unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                    await post.save();
+
+                } else {
+                    //Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                await post.save();
+                return post;
+            } else throw new UserInputError("Post not found");
         }
     }
 }
