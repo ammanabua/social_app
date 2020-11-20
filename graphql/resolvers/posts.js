c17f9247f830586182c5ac994beefac8427cc165
Amman Abua <ammanabua@gmail.com>
2020-11-20 16:25:37 +0100

    add subscription functionality to listen for new posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -41,6 +41,10 @@ module.exports = {
 
             const post = await newPost.save();
 
+            context.pubsub.publish("NEW_POST", {
+                newPost: post
+            })
+
             return post;
         },
 
@@ -58,6 +62,11 @@ module.exports = {
             } catch (err) {
                 throw new Error(err);
             }
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
         }
     }
 };
