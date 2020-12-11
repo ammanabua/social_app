7238c198eabc4430df218a9a92082c77757011fa
Amman Abua <ammanabua@gmail.com>
2020-12-11 13:29:07 +0100

    add authentication context

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import "semantic-ui-css/semantic.min.css";
 import './App.css';
 
 
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
+
+
 import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -13,14 +17,16 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-      <Container>
-      <MenuBar/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Register}/>
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+        <MenuBar/>
+        <Route exact path="/" component={Home}/>
+        <AuthRoute exact path="/login" component={Login}/>
+        <AuthRoute exact path="/register" component={Register}/>
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
+
+
+import { AuthContext, AuthProvider } from "../context/auth"
 import { useForm } from "../util/hooks"
 
 
 
 function Login(props) {
+
+    const context = useContext(AuthContext)
     const [errors, setErrors] = useState({});
     
     const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -17,7 +22,8 @@ function Login(props) {
    
 
     const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-        update(_, result){
+        update(_, { data: { login: userData }}){
+            context.login(userData)
             props.history.push("/");
         },
         onError(err){
